e35fa96f3de95ac8097a9e200aacbd3559f31d5c
CamiloLondonoDev <camilo.londono.dev@gmail.com>
2023-04-26 01:03:58 -0300

    Finalizado

--- my-project/src/Components/Tarjeta.jsx
+++ my-project/src/Components/Tarjeta.jsx
@@ -1,45 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-// const POKEAPIURL = "https://pokeapi.co/api/v2/pokemon/ditto";
-// const response = "https://rickandmortyapi.com/api/character";
-
-
-// export const Api = () =>{
-
-//   const [data, setData] =useState([])
-
-//   useEffect(()=>{
-//     PokeApi()
-//   }, [])
-
-//   async function PokeApi() {
-
-//     const response = await fetch("https://rickandmortyapi.com/api/character");
-//     const responseData = await response.json();
-
-//     setData(responseData.results)
-
-//     console.log(response);
-
-//     console.log(responseData);
-
-//   }
-
-//   return(
-//     <div>
-//       {data.map((item)=>(
-//         <div>
-//           {item.id}
-//           {item.name}
-//           <img src={...item.image} alt="" srcset="" />
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
 
 export const Api = () => {
 
@@ -48,11 +10,6 @@ export const Api = () => {
   useEffect(() => {
     GetPokeApi()
   }, [])
-
-  // async function GetPokeApi(){
-  //   const response = await axios.get("https://rickandmortyapi.com/api/character");
-  //   setData(response.data.results);
-  // };
 
   function GetPokeApi() {
     axios.get("https://rickandmortyapi.com/api/character")
@@ -64,78 +21,49 @@ export const Api = () => {
         console.log(error);
       });
   }
+
   return (
-    data.map((item) => (<Card style={{ width: '18rem' }}>
-    <Card.Img variant="top" src={item.image} />
-    <Card.Body>
-      <Card.Title><strong>Name: </strong> {item.name}</Card.Title>
-      <Card.Text>
-        <strong>Gender: </strong>
-        {item.gender}
+    data.map((item) => (<Card className="container" style={{ width: '18rem', justifyContent: 'center', marginBottom: '5px' }}>
+      <Card.Img variant="top" src={item.image} />
+      <Card.Body>
+        <Card.Title><strong>Name: </strong> {item.name}</Card.Title>
+        <Card.Text>
+          <div className="container">
+            <div className="row">
+              <div className="col-6">
+                <strong>Gender:</strong>
+              </div>
+              <div className="col-6">
+                {item.gender}
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-6">
+                <strong>Species:</strong>
+              </div>
+              <div className="col-6">
+                {item.species}
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-6">
+                <strong>Status:</strong>
+              </div>
+              <div className="col-6">
+                {item.status}
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-6">
+                <strong>Origin:</strong>
+              </div>
+              <div className="col-6">
+                {item.origin.name}
+              </div>
+            </div>
+          </div>
+        </Card.Text>
 
-        <strong>Species: </strong>
-        {item.species}
-
-        <strong>Status: </strong>
-        {item.status}
-
-        <strong>Origin: </strong>
-        {item.origin.name}
-      </Card.Text>
-    </Card.Body>
-  </Card>)));
-    
-
-      
-
-  // return(
-  //   <div>
-  //     {data.map((item)=>(
-  //       <div>
-  //         {/* {item.id} */}
-  //         {item.name}
-  //         <img src={...item.image} alt="" {...item.id} />
-  //       </div>
-  //     ))}
-  //   </div>
-  // );
+      </Card.Body>
+    </Card>)));
 }
-// export default GetPokeApi();
-
-
-// function Tarjetas() {
-
-//   return (
-//     <Card style={{ width: '18rem' }}>
-//       <Card.Img variant="top" src="holder.js/100px180" />
-//       <Card.Body>
-//         <Card.Title>Card Title</Card.Title>
-//         <Card.Text>
-//           Some quick example text to build on the card title and make up the
-//           bulk of the card's content
-//         </Card.Text>
-//         <Button variant="primary">Go somewhere</Button>
-//       </Card.Body>
-//     </Card>
-//   );
-// }
-
-// export default Tarjetas()
-
-// function BasicExample() {
-//   return (
-//     <Card style={{ width: '18rem' }}>
-//       <Card.Img variant="top" src="holder.js/100px180" />
-//       <Card.Body>
-//         <Card.Title>Card Title</Card.Title>
-//         <Card.Text>
-//           Some quick example text to build on the card title and make up the
-//           bulk of the card's content.
-//         </Card.Text>
-//         <Button variant="primary">Go somewhere</Button>
-//       </Card.Body>
-//     </Card>
-//   );
-// }
-
-// export default BasicExample;
