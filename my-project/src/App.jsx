f28b4e5f8ccc6528862e7443d5b0ca1e92f681c4
CamiloLondonoDev <camilo.londono.dev@gmail.com>
2023-04-25 22:28:18 -0300

    .

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,17 +1,15 @@
 import './styles/App.css'
 import { Card } from "react-bootstrap";
-// import Card from './Components/Tarjeta.jsx'
-import { Tarjeta } from "./Components/Tarjeta";
-// import { getPokeApi } from "./Components/Tarjeta";
+// import { Api } from "./Components/Tarjeta";
+import Api from "./Components/Tarjeta";
 
 
 function App() {
 
   return (
     <>
-      <Tarjeta/>
       <div>
-        <Card />
+        <Api/>
       </div>
 
     </>
--- my-project/src/Components/Tarjeta.jsx
+++ my-project/src/Components/Tarjeta.jsx
@@ -1,42 +1,103 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import axios from "axios";
 
 // const POKEAPIURL = "https://pokeapi.co/api/v2/pokemon/ditto";
-const POKEAPIURL = "https://rickandmortyapi.com/api/character";
+// const response = "https://rickandmortyapi.com/api/character";
 
 
-export default function getPokeApi(){
-  axios.get("https://rickandmortyapi.com/api/character")
+// export const Api = () =>{
 
-  .then((response)=>{
-    console.log(POKEAPIURL);
+//   const [data, setData] =useState([])
 
-    setData(response.data.results);
-  })
-  .catch((error)=>{
-    console.log(error);
-  });
-}
+//   useEffect(()=>{
+//     PokeApi()
+//   }, [])
 
+//   async function PokeApi() {
 
+//     const response = await fetch("https://rickandmortyapi.com/api/character");
+//     const responseData = await response.json();
 
-export default function Tarjetas() {
+//     setData(responseData.results)
 
-  return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+//     console.log(response);
+
+//     console.log(responseData);
+
+//   }
+
+//   return(
+//     <div>
+//       {data.map((item)=>(
+//         <div>
+//           {item.id}
+//           {item.name}
+//           <img src={...item.image} alt="" srcset="" />
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+export const Api = () => {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    GetPokeApi()
+  }, [])
+
+  // async function GetPokeApi(){
+  //   const response = await axios.get("https://rickandmortyapi.com/api/character");
+  //   setData(response.data.results);
+  // };
+
+  function GetPokeApi() {
+    axios.get("https://rickandmortyapi.com/api/character")
+      .then((response) => {
+        setData(response.data.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+ 
+
+  return(
+    <div>
+      {data.map((item)=>(
+        <div>
+          {item.id}
+          {item.name}
+          <img src={...item.image} alt="" srcset="" />
+        </div>
+      ))}
+    </div>
   );
 }
+// export default GetPokeApi();
+
+
+// function Tarjetas() {
+
+//   return (
+//     <Card style={{ width: '18rem' }}>
+//       <Card.Img variant="top" src="holder.js/100px180" />
+//       <Card.Body>
+//         <Card.Title>Card Title</Card.Title>
+//         <Card.Text>
+//           Some quick example text to build on the card title and make up the
+//           bulk of the card's content
+//         </Card.Text>
+//         <Button variant="primary">Go somewhere</Button>
+//       </Card.Body>
+//     </Card>
+//   );
+// }
+
+// export default Tarjetas()
 
 // function BasicExample() {
 //   return (
